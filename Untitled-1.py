82150518bbb533876ef9a3d9d9954a4647fced00
ROTIN37 <rostohar.tin@gmail.com>
2022-12-23 20:08:20 +0100

    Just more changes

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+
+
+#with open('C:\Users\ROTIN37\Documents\Website\iotblog7b.github.io\info.txt', 'a') as "info.txt":
+#    "info.txt".write('Hello\n')
+
+with open('info.txt', 'w') as f:
+    f.write('Working?\n')
