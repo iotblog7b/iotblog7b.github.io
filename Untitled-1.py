75558efe739955f76db20a21cb702514031cf5c7
ROTIN37 <rostohar.tin@gmail.com>
2023-03-22 15:47:01 +0100

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,4 @@
 
 
-#with open('C:\Users\ROTIN37\Documents\Website\iotblog7b.github.io\info.txt', 'a') as "info.txt":
-#    "info.txt".write('Hello\n')
-
-with open('info.txt', 'w') as f:
-    f.write('Working?\n')
+import sqlite3
+con = sqlite3.connect("tutorial.db")
