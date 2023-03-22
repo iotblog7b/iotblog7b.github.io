34f06f21aabae17925f58e821d9c5ecdf6ba3541
ROTIN37 <rostohar.tin@gmail.com>
2023-03-22 17:36:02 +0100

    Update

--- scriptLive.js
+++ scriptLive.js
@@ -1,1 +1,11 @@
 
+const sqlite3 = require('sqlite3').verbose();
+
+// Open a database connection
+const db = new sqlite3.Database('mydb.sqlite');
+
+// Create a table
+db.run('CREATE TABLE users (id INT, name TEXT)');
+
+// Close the database connection
+db.close();
