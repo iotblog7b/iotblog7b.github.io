4fa0edd481c42a64dd09c77c000b0a9c5720688f
iotblog7b <120745747+iotblog7b@users.noreply.github.com>
2023-04-09 14:25:50 +0200

    Update buble.js

--- buble.js
+++ buble.js
@@ -1,1 +1,24 @@
+const animateBubble = x => {
+  const bubble = document.createElement("div");
+  
+  bubble.className = "bubble";
+  
+  bubble.style.left = '${x}px';
+  
+  wrapper.appendChild(bubble);
+  
+  setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX);
+
+
+
+
+
+
+
+
+
+
 
